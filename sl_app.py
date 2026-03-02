e5582a7e2962be4d5a10a06c65e1328b167fdaa2
Tammy Scheffer <tammy.scheffer@gmail.com>
2026-03-01 20:45:08 -0500

    Improved layout, removed unneeded lines

--- sl_app.py
+++ sl_app.py
@@ -20,21 +20,21 @@ def key_selection(widgt_key: int) -> mu.key.Key:
 
     with col1:
         chosen_rootnote_letter = st.selectbox(
-            "Choose the root note:",
+            ":grey[Tonic (first note in scale)]",
             (note_letters),
             key=widgt_key+"a"
         )
     
     with col2:
         chosen_accidental = st.selectbox(
-            "Choose an accidental:",
+            ":grey[Accidental]",
             (note_accidentals),
             key=widgt_key+"b"
         )
         
     with col3:
         chosen_quality = st.selectbox(
-            "Choose a key quality:",
+            ":grey[Key Quality]",
             (key_quality),
             key=widgt_key+"c"
         )
@@ -49,21 +49,21 @@ def note_selection(widgt_key):
 
     with col1:
         chosen_note_letter = st.selectbox(
-            "Choose a note:",
+            ":grey[Note]",
             (note_letters),
             key=widgt_key+"a"
         )
     
     with col2:
         chosen_accidental = st.selectbox(
-            "Choose an accidental:",
+            ":grey[Accidental]",
             (note_accidentals),
             key=widgt_key+"b"
         )
         
     with col3:
         chosen_octave = st.selectbox(
-            "Choose an octave:",
+            ":grey[Octave]",
             (note_octaves),
             index=3,
             key=widgt_key+"c"
@@ -72,34 +72,61 @@ def note_selection(widgt_key):
     chosen_full_note = chosen_note_letter + chosen_accidental + chosen_octave
     return mu.note.Note(chosen_full_note)
 
+st.title("Melodic Range Transposition Tool")
 
-st.write("Choose the lowest note in the song:")
-start_bottom_note = note_selection(1)
+st.write(
+    """Use this tool to input the key, bottom note and top note in the melody. Then use
+    the slider below to find a new key with an appropriate melodic range.
+    """
+)
 
-st.write("Choose the highest note in the song:")
-start_top_note = note_selection(2)
+st.write("---")
 
-st.write("Choose the key this song is in:")
-start_key = key_selection(3)
+col1, col2 = st.columns(spec=[0.7, 0.3])
+
+with col1:
+    st.write("#### Starting Key")
+    start_key = key_selection(1)
+
+st.write("")
+
+st.write("#### Melodic Range")
+col1, col2 = st.columns(2, gap="large")
+
+with col1:
+    st.write("Lowest melody note")
+    start_bottom_note = note_selection(2)
+
+with col2:
+    st.write("Highest melody note")
+    start_top_note = note_selection(3)
+
+st.write("")
+st.write("")
 
 # TODO: add a friendly readable representation of start_key
 st.write(
-    f"\nThis song is in the key of {start_key}, the bottom note of the melody is " + \
-    f"{start_bottom_note.pitch.unicodeNameWithOctave}, and the top note is " + \
-    f"{start_top_note.pitch.unicodeNameWithOctave}."
+    f"""This song is in the key of :primary[**{start_key.tonic.unicodeName} {start_key.mode}**], 
+    the bottom note of the melody is :primary[**{start_bottom_note.pitch.unicodeNameWithOctave}**], 
+    and the top note is :primary[**{start_top_note.pitch.unicodeNameWithOctave}**]
+    """
 )
 
-by_halfsteps = st.slider("Transpose by how many half steps?", -11, 11, 0)
-direction = "-" if by_halfsteps < 0 else "+"
+st.write("---")
 
+st.write("#### Transposition")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    by_halfsteps = st.slider(":grey[Transpose up or down by half steps]", -11, 11, 0)
+    direction = "-" if by_halfsteps < 0 else "+"
 
 new_key = start_key.transpose(by_halfsteps)
-st.write(f"The new key is {new_key}")
 
 # check for mismatch between interval specified in half steps and found interval 
 # if mismatched, correct using complement interval
-
-st.write("\nI will now make some calculations to make sure my transposition is correct!")
+# TODO: organize the transposition verfication in a function
 
 root_start_key = start_key.tonic
 root_new_key = new_key.tonic
@@ -107,41 +134,31 @@ found_interval = mu.interval.Interval(root_start_key, root_new_key) # find inter
 found_interval_in_halfsteps = found_interval.semitones # extract number of half steps in found interval
 
 if by_halfsteps != found_interval_in_halfsteps:
-    st.write("Seems the interval direction got flipped! Let me fix that real quick...")
-    found_interval_complement = found_interval.complement
-    st.write(f"I'm changing {found_interval.niceName} to {found_interval_complement.niceName}")
-    found_interval = found_interval_complement
-
-# checking the atributes of the interval
-st.write(f"{found_interval.direction=}, {found_interval.directedName=}, {found_interval.directedNiceName=}")
+    found_interval = found_interval.complement
 
 # add descending direction to interval if not already
 
 if direction == "-" and found_interval.direction == 1:
-    st.write(f"the interval I have is {found_interval.directedNiceName}, but it should be descending")
     found_interval = found_interval.reverse()
-    st.write(f"This interval was not set to descending, and now I changed it to {found_interval.directedNiceName}.")
 
 # as far as we know, and interval would always be ascending unless specified otherwise. However,
 # below is a test just in case
+
 if direction == "+" and found_interval.direction == -1:
-    st.write(f"the interval I have is {found_interval.directedNiceName}, but it should be ascending")
     found_interval = found_interval.reverse()
-    st.write(f"This interval was not set to ascending, and now I changed it to {found_interval.directedNiceName}.")
-# else:
-#     found_interval.direction = 1
-#     print(f"This interval is ascending, so it's {found_interval}")
-
-st.write(f"I am transposing by {by_halfsteps} half steps, or {found_interval.directedNiceName}.")
-
-new_bottom_note = start_bottom_note.transpose(found_interval)
-new__top_note = start_top_note.transpose(found_interval)
 
 
-st.write(
-    f"In the key of {new_key}, the low note in the melody would be " + \
-    f"{new_bottom_note.pitch.unicodeNameWithOctave}, and the high note would be " + \
-    f"{new__top_note.pitch.unicodeNameWithOctave}."
-)
+if by_halfsteps != 0:
+    st.write(f"Transpose by :primary[**{by_halfsteps}**] half steps, or a :primary[**{found_interval.directedNiceName}**]")
+    new_bottom_note = start_bottom_note.transpose(found_interval)
+    new__top_note = start_top_note.transpose(found_interval)
+    st.write("---")
+    st.write(
+        f"##### In the key of :primary[**{new_key.tonic.unicodeName} {new_key.mode}**], the bottom " + \
+        f"note of the melody is :primary[**{new_bottom_note.pitch.unicodeNameWithOctave}**], and the " + \
+        f"top note is :primary[**{new__top_note.pitch.unicodeNameWithOctave}**]"
+    )
 
-st.write("Goodbye!")
+else:
+    st.write("")
+    st.write("---")
